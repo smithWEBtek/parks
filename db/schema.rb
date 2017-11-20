66e35d7370ef57d9f26a27e8eb626feeddfc3a5a
Scott Savage <snsavage@gmail.com>
2017-11-19 20:25:29 -0500

    Create State model and park associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120005329) do
+ActiveRecord::Schema.define(version: 20171120012211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20171120005329) do
     t.string "full_name"
     t.string "url"
     t.text "weather_info"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "states", force: :cascade do |t|
+    t.string "abbreviation"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
